f6a898d83daef84e36a383f741be18c806e8faec
Nathanaël Couret <nathanael.couret@gbandsmith.com>
2018-11-15 17:50:33 +0100

    Some documentation

--- readme.md
+++ readme.md
@@ -6,3 +6,17 @@ A cron parser/generator with support for multiple cron format.
 ## Usage
 
 Coming soon
+
+## scripts
+
+> build
+
+Build into a ES5 app.
+
+> test
+
+Run unit tests
+
+> run
+
+Run the app. Output files can be found in the dist directory
--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,11 @@ import { writeFileSync } from "fs";
 
 // Just doing lots of testing in there
 const parser = new CronParser();
+// Parse the input into tokens
 const lexingResult = CronLexer.tokenize("0 0 12 ? * 5L *");
 parser.input = lexingResult.tokens;
 
+// Parse the tokens into a CST
 const cst = parser.cronExpression();
 
 if (parser.errors.length > 0) {
@@ -20,15 +22,17 @@ if (parser.errors.length > 0) {
   console.log("oh yeah");
 }
 
+// Semantic analysis
 const ast = cronVisitor.visit(cst);
 
+// Grammar diagram generation 
 const serializedGrammar = parser.getSerializedGastProductions();
-
 // create the HTML Text
 const htmlText = createSyntaxDiagramsCode(serializedGrammar);
-
 // Write the HTML file to disk
 const outPath = resolve(__dirname, "./");
 writeFileSync(outPath + "/generated_diagrams.html", htmlText);
+
 writeFileSync(outPath + "/ast.json",JSON.stringify(ast));
+// Generate the cron expression from the AST
 writeFileSync(outPath + "/cron.txt", ast.value());
