93ecaf6223332b8895dab27aac264d69a6a7b315
Nathanaël COURET <nathanael.couret@ensimag.grenoble-inp.fr>
2019-01-28 09:30:01 +0100

    grammar generation for all 3 parsers

--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
 import { createSyntaxDiagramsCode } from "chevrotain";
-import { writeFileSync } from "fs";
-import { resolve } from "path";
-import { baseVocabulary, CronLexer } from "./src/lexer";
-import { BaseParser } from "./src/parser";
+import { writeFile, writeFileSync } from "fs";
+import { baseVocabulary } from "./src/lexer";
+import { BaseParser, JenkinsParser, QuartzParser } from "./src/parser";
 
 // Just doing lots of testing in there
-const parser = new BaseParser(baseVocabulary, false);
-// Parse the input into tokens
-const lexingResult = CronLexer.tokenize("5 4 * * *");
-console.log(lexingResult);
-parser.input = lexingResult.tokens;
+const quartzParser = new QuartzParser();
+const cronParser = new BaseParser(baseVocabulary);
+const jenkinsParser = new JenkinsParser();
 
 // Parse the tokens into a CST
-const cst = parser.cronExpression();
+//parser.cronExpression();
 
 // Grammar diagram generation
-const serializedGrammar = parser.getSerializedGastProductions();
-// create the HTML Text
-const htmlText = createSyntaxDiagramsCode(serializedGrammar);
+const quartzGrammar = quartzParser.getSerializedGastProductions();
+const cronGrammar = cronParser.getSerializedGastProductions();
+const jenkinsGrammar = jenkinsParser.getSerializedGastProductions();
 // Write the HTML file to disk
-writeFileSync("./generated_diagrams.html", htmlText);
+writeFileSync("./grammar/quartz.html", createSyntaxDiagramsCode(quartzGrammar));
+writeFileSync("./grammar/cron.html", createSyntaxDiagramsCode(cronGrammar));
+writeFileSync("./grammar/jenkins.html", createSyntaxDiagramsCode(jenkinsGrammar));
+
 
 // Semantic analysis
-// const ast = cronVisitor.visit(cst);
+//const ast = cronVisitor.visit(cst);
 
-// writeFileSync(outPath + "/ast.json", JSON.stringify(ast));
+//writeFileSync("/ast.json", JSON.stringify(ast));
 // // Generate the cron expression from the AST
 // writeFileSync(outPath + "/cron.txt", ast.value());
