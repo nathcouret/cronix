e8d6af9932eb7e76d4953f8cf1cb43e0431ee29a
Nathanaël COURET <nathanael.couret@ensimag.grenoble-inp.fr>
2019-02-07 19:08:42 +0100

    Better API with tests

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,17 @@
-interface CronExpr {
+import { baseVocabulary, CronLexer } from "./lexer";
+import { BaseParser } from "./parser";
+import { BaseVisitor } from "./semantic/BaseVisitor";
+import { CronExpression } from "./syntax/syntax";
+
+// default values to * to reduce boilerplate for the user
+// second and year optionals, only used for Quartz scheduler
+export interface CronExpr {
   second?: string;
-  minute: string;
-  hour: string;
-  dayOfMonth: string;
-  month: string;
-  dayOfWeek: string;
+  minute?: string;
+  hour?: string;
+  dayOfMonth?: string;
+  month?: string;
+  dayOfWeek?: string;
   year?: string;
 }
 
@@ -18,51 +25,58 @@ interface CronOptions {
   mode: CronMode;
 }
 
-export function cron(
-  expression: string | CronExpr,
-  options: CronOptions = { mode: CronMode.CRONTAB }
-) {
-  let expr;
+export function cron(expression: string | CronExpr, options: CronOptions = { mode: CronMode.CRONTAB }) {
+  let expr: string;
   if (isCronException(expression)) {
-    expr = expression;
-  } else {
     expr = compute(expression, options);
+  } else {
+    expr = expression;
   }
-  return expr; /* smth */
+  return computeExpr(expr, options);
 }
 
-function isCronException(
-  expression: string | CronExpr
-): expression is CronExpr {
+function isCronException(expression: string | CronExpr): expression is CronExpr {
   return (<CronExpr>expression).hour !== undefined;
 }
 
-function compute(expression: string, options: CronOptions): CronExpr {
-  const splits = (expression as string).split(" ");
+function computeExpr(expr: string, options: CronOptions): CronExpression {
   const { mode } = options;
   switch (mode) {
     case CronMode.QUARTZ:
-      const computed: CronExpr = {
-        second: splits[0],
-        minute: splits[1],
-        hour: splits[2],
-        dayOfMonth: splits[3],
-        month: splits[4],
-        dayOfWeek: splits[5]
-      };
-      if (splits.length >= 7) {
-        computed.year = splits[6];
-      }
-      return computed;
     case CronMode.JENKINS:
     case CronMode.CRONTAB:
     default:
-      return {
-        minute: splits[0],
-        hour: splits[1],
-        dayOfMonth: splits[2],
-        month: splits[3],
-        dayOfWeek: splits[4]
-      };
+      const lexingResult = CronLexer.tokenize(expr);
+      const parser = new BaseParser(baseVocabulary);
+      parser.input = lexingResult.tokens;
+      const cst = parser.cron();
+      return new BaseVisitor().visit(cst);
+  }
+}
+
+function compute(expression: CronExpr, options: CronOptions): string {
+  const { mode } = options;
+  // default values to * to reduce boilerplate for the user
+  const base = [
+    expression.minute || "*",
+    expression.hour || "*",
+    expression.dayOfMonth || "*",
+    expression.month || "*",
+    expression.dayOfWeek || "*"
+  ];
+  switch (mode) {
+    case CronMode.QUARTZ:
+      let expr = base;
+      if (expression.second) {
+        expr = [expression.second].concat(base);
+      }
+      if (expression.year) {
+        expr.push(expression.year);
+      }
+      return expr.join(" ");
+    case CronMode.JENKINS:
+    case CronMode.CRONTAB:
+    default:
+      return base.join(" ");
   }
 }
---
+++ test/api.test.ts
@@ -0,0 +1,34 @@
+import { cron, CronExpr } from "../src/api";
+
+describe("compute", () => {
+  test("A simple Crontab expression", () => {
+    // Given
+    // Everyday at 04:05
+    const expression: CronExpr = {
+      minute: "5",
+      hour: "4"
+    };
+    // When
+    const parsed = cron(expression);
+    // Then
+    expect(parsed.minute.value()).toBe("5");
+    expect(parsed.hour.value()).toBe("4");
+    expect(parsed.dow.value()).toBe("*");
+    expect(parsed.month.value()).toBe("*");
+    expect(parsed.dom.value()).toBe("*");
+  });
+
+  test("String expression compute ok", () => {
+    // Given
+    // Everyday at 04:05
+    const expression = "5 4 * * *";
+    // When
+    const parsed = cron(expression);
+    // Then
+    expect(parsed.minute.value()).toBe("5");
+    expect(parsed.hour.value()).toBe("4");
+    expect(parsed.dow.value()).toBe("*");
+    expect(parsed.month.value()).toBe("*");
+    expect(parsed.dom.value()).toBe("*");
+  });
+});
