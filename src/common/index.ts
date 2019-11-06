5545a8a5b1eba11bd83f2d32c1e5e772dec19567
Nathanael Couret <nathanael.couret@gmail.com>
2019-11-06 13:37:08 +0100

    Add missing export for common syntax

--- src/common/index.ts
+++ src/common/index.ts
@@ -3,5 +3,6 @@ export * from "./lexer";
 export * from "./parser";
 export * from "./AbstractVisitorConstructor";
 export * from "./context";
+export * from "./syntax";
 
 export { default as AbstractVisitorConstructor } from "./AbstractVisitorConstructor";
